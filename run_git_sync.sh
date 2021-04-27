f5ab83cb1b7fdef7d631d0b2fb6e571c62879cf3
Your Name <you@example.com>
2021-04-27 14:17:48 +0200

    wip

--- run_git_sync.sh
+++ run_git_sync.sh
@@ -1,13 +1,14 @@
 echo "Reading configuration..."
+
 read username < username.txt
 read token < token.txt
 read repository < repository.txt
 read location < location.txt
 
-echo "Executing git sync"
-CMD_STRING = "git pull https://${token}@github.com/${username}/${repository}"
+echo "Executing git sync..."
 
-cd location
+cd ${location}
 
-eval $CMD_STRING
+eval "git pull https://${token}@github.com/${username}/${repository}"
+
 echo "Done! Thank you."
